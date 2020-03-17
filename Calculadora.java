b144b7317bcfba93161297e0440633149f74b594
Fernando Emercini <fernando_emercini@outlook.com>
2020-03-17 00:39:00 -0300

    Calcula soma, subtrai, multiplica e divide

--- Calculadora.java
+++ Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora
 {
    public static void main(String[] args)
    {
-      int n1, n2, soma, subtracao;
+      int n1, n2, soma, subtracao, multiplicacao, divisao;
       
       n1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o primeiro numero: "));
       n2 = Integer.parseInt(JOptionPane.showInputDialog("Informe o segundo numero: "));
@@ -16,5 +16,14 @@ public class Calculadora
       subtracao = n1 - n2;
       
       System.out.println("A subtracao dos dois numeros eh: " + subtracao);
+      
+      multiplicacao = n1 * n2;
+      
+      System.out.println("A multiplicacao dos dois numeros eh: " + multiplicacao);
+      
+      divisao = n1 / n2;
+      
+      System.out.println("A divisao dos dois numeros eh: " + divisao);
+      
    }
 }
